fee894b7fcf69ed10e159adf5cdb09c8b1dfedab
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2015-02-14 13:30:17 -0500

    Reenable high-res capes

--- writecape.rb
+++ writecape.rb
@@ -7,6 +7,10 @@ def writecape(file, outpath)
 		self.format = "PNG"
 		self.background_color = "transparent"
 	}[0]
+	if img.columns >= 64 and img.rows >= 32
+		File.copy_stream(file, outpath)
+		return
+	end
 	img2 = img.extent(64, 32)
 	img2.write(outpath)
 end
